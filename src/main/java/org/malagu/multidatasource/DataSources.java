9c83b39f0d907ed649e11d2a60096f0547270df7
kevin <kevin.yang@codemune.com>
2017-11-23 22:33:06 +0800

    首次提交

---
+++ src/main/java/org/malagu/multidatasource/DataSources.java
@@ -0,0 +1,16 @@
+package org.malagu.multidatasource;
+
+/**
+ * @author Kevin Yang (mailto:muxiangqiu@gmail.com)
+ * @since 2017年11月23日
+ */
+public abstract class DataSources {
+
+	public static final String dataSource = "dataSource";
+	public static final String dataSource1 = "dataSource1";
+	public static final String dataSource2 = "dataSource2";
+	public static final String dataSource3 = "dataSource3";
+	public static final String dataSource4 = "dataSource4";
+	public static final String dataSource5 = "dataSource5";
+	public static final String dataSource6 = "dataSource6";
+}
